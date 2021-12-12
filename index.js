89c5db671ce7fcd69962ef781a333c1caacf7e31
kelvinfernandes <kelvin.fernandes.sio@gmail.com>
2021-12-12 19:10:44 +0100

    ajout de index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const Datastore = require('nedb');
+
+
+
+const app = express();
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if(err){
+            response.end();
+            return;
+        }
+       response.json(data);
+    });  
+});
+
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json({
+        status: 'success',
+        timestamp: timestamp,
+        nom_tournage: data.nom_tournage,
+        ardt_lieu: data.ardt_lieu,
+        date_fin: data.date_fin,
+    });
+});
